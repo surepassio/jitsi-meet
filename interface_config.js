fa2440d3263b57d9a20252781f7226f67c80f13d
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-06-10 18:42:11 +0530

    change notfication dismiss timeout

--- interface_config.js
+++ interface_config.js
@@ -240,7 +240,7 @@ var interfaceConfig = {
      * explicitly dismissed through a user action. The value is how long, in
      * milliseconds, those notifications should remain displayed.
      */
-    // ENFORCE_NOTIFICATION_AUTO_DISMISS_TIMEOUT: 15000,
+    ENFORCE_NOTIFICATION_AUTO_DISMISS_TIMEOUT: 100,
 
     // List of undocumented settings
     /**
